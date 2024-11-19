08d086b301a62d9a1c8bf0d66051c793305458a9
Jhonnatan Gomez <jalfgomez@poligran.edu.co>
2024-11-19 00:16:06 -0500

    Se complementa page de SelectSesion, agregando temas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,7 @@ export const routes: Routes = [
   },  
   {
     path: 'playback',
-    loadComponent: () => import('./playback/playback.page').then( m => m.Playback)
+    loadComponent: () => import('./playback/playback.page').then( m => m.PlaybackPage)
   },
   {
     path: 'selectSesion',
--- src/app/selectSesion/selectSesion.page.ts
+++ src/app/selectSesion/selectSesion.page.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -15,7 +16,51 @@ import { ModalController } from '@ionic/angular';
 export class SelectSesion implements OnInit {
   selectedDate: string = '';
   selectedTime: string = '';
-  isModalOpen: boolean = false;
+  isModalOpen: boolean = false; 
+  selectedSession: any = null; //sesion seleccionada
+  isConfirmActive: boolean = false;// estado del boton Confirmar
+
+  //Lista de sessiones
+
+  sessions = [
+    {
+      title: 'Relajación Profunda',
+      objective: 'Reducir el estrés y liberar tensiones acumuladas en el cuerpo.',
+      image: 'assets/images/img-Relajacion.jpg',
+      selected: false,
+    },
+    {
+      title: 'Cultivar la Gratitud',
+      objective: 'Desarrollar una mentalidad positiva y reconocer las cosas buenas en tu vida.',
+      image: 'assets/images/img-gratitud.jpg',
+      selected: false,
+    },
+    {
+      title: 'Atención Plena (Mindfulness)',
+      objective: 'Vivir el momento presente con plena conciencia y sin juicio.',
+      image: 'assets/images/img-mindfulness.jpg',
+      selected: false,
+    },
+    {
+      title: 'Autoaceptación',
+      objective: 'Promover una relación más amable contigo mismo y fortalecer la autoestima.',
+      image: 'assets/images/img-autoaceptacion.jpg',
+      selected: false,
+    },
+  ];
+
+  //seleccionar una sesion
+  selectSession(session: any) {
+    this.sessions.forEach ((s) => (s.selected = false)); //deselecciona todas las sesiones
+    session.selected = true;
+    this.selectedSession = session;
+    this.updateConfirmState();
+  }
+
+  updateConfirmState() {
+    this.isConfirmActive = !!this.selectedDate && !!this.selectedTime && !!this.selectedSession;
+  }
+
 
   constructor(private router: Router, private modalController: ModalController) {}
 
@@ -28,10 +73,10 @@ export class SelectSesion implements OnInit {
   }
 
   confirmSelection() {
-    if (this.selectedDate && this.selectedTime) {
-      // Abre el modal solo si hay valores seleccionados
-      this.isModalOpen = true;
-    }
+    // Abre el modal solo si hay valores seleccionados
+    this.isModalOpen = true;
+    console.log('Fecha:', this.selectedDate);
+    console.log('Hora:', this.selectedTime);
   }
 
   closeModal() {
@@ -41,7 +86,10 @@ export class SelectSesion implements OnInit {
   resetFields() {
     this.selectedDate = '';
     this.selectedTime = '';
-    this.isModalOpen = false;
+    this.selectedSession = '';
+    this.sessions.forEach ((s) => (s.selected = false));
+    this.updateConfirmState();
+    this.closeModal();
   }
 
   ngOnInit() {}
